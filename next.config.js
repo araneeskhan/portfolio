4065a428ce3e8295f371bfab03de57fa7e9c6210
Anees Ur Rehman <aneesurrehman1358@gmail.com>
2026-01-27 10:10:22 +0500

    fix: remove redirects for static export

--- next.config.js
+++ next.config.js
@@ -1,20 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/portfolio',
+  output: "export",
+  basePath: "/portfolio",
   images: {
     unoptimized: true,
-  },
-  
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/portfolio',
-        basePath: false,
-        permanent: true,
-      },
-    ];
   },
 };
 
