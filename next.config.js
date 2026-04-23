087512b224cd462a9a3fcec3f4c275161182d82f
Anees Ur Rehman <aneesurrehman1358@gmail.com>
2026-04-23 14:45:11 +0500

    Fixed Minor Bugs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   output: "export",
   trailingSlash: true,
@@ -9,12 +7,6 @@ const nextConfig = {
   images: { unoptimized: true },
   env: {
     NEXT_PUBLIC_BASE_PATH: "/portfolio",
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.output.publicPath = "/portfolio/_next/";
-    }
-    return config;
   },
 };
 
