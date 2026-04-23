7d648b67cb72507b5a9b7d516595b5441d400b01
Anees Ur Rehman <aneesurrehman1358@gmail.com>
2026-04-23 14:30:29 +0500

    Fixed Minor Bugs

--- next.config.js
+++ next.config.js
@@ -2,14 +2,13 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  output: "export", // REQUIRED for GitHub Pages
+  output: "export", 
   basePath: "/portfolio",
   assetPrefix: "/portfolio",
   images: {
     unoptimized: true,
   },
   env: {
-    // Hardcoding this for now to ensure it NEVER returns undefined
     NEXT_PUBLIC_BASE_PATH: "/portfolio",
   },
 };
