f9e392d983f0baf7c63487246e66e5a29986c151
Anees Ur Rehman <aneesurrehman1358@gmail.com>
2026-04-23 06:46:12 +0500

    Fix Vercel build script and update Next.js

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  output: "export",
+  output: process.env.BUILD_STANDALONE === "true" ? "standalone" : undefined,
+  images: {
+    unoptimized: true,
+  },
   basePath: isProd ? "/portfolio" : "",
   assetPrefix: isProd ? "/portfolio" : "",
   env: {
     NEXT_PUBLIC_BASE_PATH: isProd ? "/portfolio" : "",
-  },
-  images: {
-    unoptimized: true,
   },
 };
 
