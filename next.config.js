f4f83c5a848d91a5ccf83152f2d05db094d0d358
Anees Ur Rehman <aneesurrehman1358@gmail.com>
2026-04-23 06:07:18 +0500

    Added Docker and CI/CD Pipeline

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  output: "export",
+  output: "standalone",
   basePath: isProd ? "/portfolio" : "",
   assetPrefix: isProd ? "/portfolio" : "",
   env: {
