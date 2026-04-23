fef70886ace249f7712e4753c300ee08df8a8b8f
Anees Ur Rehman <aneesurrehman1358@gmail.com>
2026-04-23 06:57:14 +0500

    Removed all GitHub Pages pathing

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   output: process.env.BUILD_STANDALONE === "true" ? "standalone" : undefined,
   images: {
     unoptimized: true,
-  },
-  basePath: isProd ? "/portfolio" : "",
-  assetPrefix: isProd ? "/portfolio" : "",
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? "/portfolio" : "",
   },
 };
 
